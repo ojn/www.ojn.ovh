162b97d7d49bed77f7f1cc0f70753d0efdadcf85
ojn <ojn@mailbox.org>
2026-03-18 13:40:37 +0100

    merging

--- docs/searcher-c2a407aa.js
+++ docs/searcher-c2a407aa.js
@@ -437,7 +437,7 @@ window.search = window.search || {};
         if (yes) {
             loadSearchScript(
                 window.path_to_searchindex_js ||
-                path_to_root + 'searchindex-4c1bcd61.js',
+                path_to_root + 'searchindex-e017e75f.js',
                 'mdbook-search-index');
             search_wrap.classList.remove('hidden');
             searchicon.setAttribute('aria-expanded', 'true');
