8288e84eadd69a70b66bc00b3be0201fe3937e9b
ojn <ojn@mailbox.org>
2026-03-21 12:17:23 +0100

    QOwnNotes commit

--- docs/searcher-c2a407aa.js
+++ docs/searcher-c2a407aa.js
@@ -437,7 +437,7 @@ window.search = window.search || {};
         if (yes) {
             loadSearchScript(
                 window.path_to_searchindex_js ||
-                path_to_root + 'searchindex-9213d0d4.js',
+                path_to_root + 'searchindex-b499908d.js',
                 'mdbook-search-index');
             search_wrap.classList.remove('hidden');
             searchicon.setAttribute('aria-expanded', 'true');
